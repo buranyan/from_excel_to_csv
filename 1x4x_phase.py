75619efc76dd524f7d1faa8a3a53433a41bd0327
buranyan <9108hani@gmail.com>
2025-02-19 20:38:24 +0900

    update

--- 1x4x_phase.py
+++ 1x4x_phase.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Simulation settings
-num_steps = 5000  # Time steps
+num_steps = 4096  # Time steps
 threshold_4x = 1023  # Maximum value for 4X (10 bits)
 threshold_1x = 4095  # Maximum value for 1X (12 bits)
 
 # Offset angle settings
-offset_degrees = -10  
-offset_1x = int(threshold_4x * (offset_degrees / 90))
+offset_degrees = 1 # +:lead, -:lag  
+offset_1x = int(threshold_1x * (offset_degrees / 360))
 if offset_1x < 0:
     offset_1x = 4096 + offset_1x 
 
@@ -43,13 +43,13 @@ for step in range(num_steps):
 # Plotting
 plt.figure(figsize=(10, 5))
 plt.plot(theta_4x, label="4X (10bit)", linestyle='-', marker='o', markersize=1)
-plt.plot(theta_1x, label="1X (2bit)", linestyle='--', marker='s', markersize=1)
+plt.plot(theta_1x, label="1X (12bit)", linestyle='--', marker='s', markersize=1)
 plt.plot(combined, label="Combined (12bit)", linestyle='-', marker='d', markersize=1, color='g')
 
 plt.axhline(y=threshold_4x, color='gray', linestyle='dotted', label="90° (1023)")
 plt.xlabel("Time (Steps)")
-plt.ylabel("Angle (Unit: 4X)")
+plt.ylabel("Angle (Unit: LSB)")
 plt.legend()
-plt.title(f"1X Phase Offset Simulation ({offset_degrees}° offset)")
+plt.title(f"1X Phase Offset ({offset_degrees}° offset)")
 plt.grid()
 plt.show()
